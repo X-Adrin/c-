78040cc49c46dbb751daed3f8209c295cc101c4f
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2022-04-15 22:48:07 -0300

    exemplo basico

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+
+    static void Main(){
+Console.WriteLine("Hello, World!");
+int l;   // define variavel
+l=10;
+Console.WriteLine(l);
+int m;
+
+for (m=1;m<100; m++) {  // conta  até 100
+
+    Console.WriteLine(m);
+ 
+};
+
+string [] nomes ={"maria","jose"};  // coloca valores no array
+
+for (int x=0; x<2 ;x++){
+    Console.WriteLine(nomes[x]); //varre o array
+};
+
+
+
+    
+
+}
+
+}
